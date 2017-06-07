2b1ae59c5dfee8c7c8b3aa68f35d5ed6beb48812
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2017-06-06 20:17:42 -0700

    fixed tabs to be 2 spaces

--- src/components/viewController/ViewController.js
+++ src/components/viewController/ViewController.js
@@ -5,44 +5,46 @@ import BookDetails from '../bookDetails/BookDetails';
 import {updateSelectedBook,clearSelectedBook,decrementPageNumber,incrementPageNumber} from '../../actions/books';
 
 class ViewController extends Component {
-    getListOrDetails(selectedBook) {
-        if (selectedBook) {
-            return (<BookDetails 
-                    book={selectedBook} 
-                    clearSelectedBook={this.props.clearSelectedBook} 
-                    decrementPageNumber={this.props.decrementPageNumber} 
-                    incrementPageNumber={this.props.incrementPageNumber}
-                />);
-        } else {
-            return (<BooksList 
-                        books={this.props.books} 
-                        updateSelectedBook={this.props.updateSelectedBook}
-                   />);
-        }
+
+  getListOrDetails(selectedBook) {
+    if (selectedBook) {
+      return (<BookDetails 
+                book={selectedBook} 
+                clearSelectedBook={this.props.clearSelectedBook} 
+                decrementPageNumber={this.props.decrementPageNumber} 
+                incrementPageNumber={this.props.incrementPageNumber}
+              />);
+    } else {
+      return (<BooksList 
+                books={this.props.books} 
+                updateSelectedBook={this.props.updateSelectedBook}
+              />);
     }
+  }
     
-    render() {
-        const selectedBook = this.props.books.filter((book) => book.isbn === this.props.selected)[0];
-        return (
-            <div>
-                {this.getListOrDetails(selectedBook)}
-            </div>
-        );     
-    }
+  render() {
+    const selectedBook = this.props.books.filter((book) => book.isbn === this.props.selected)[0];
+    return (
+      <div>
+        {this.getListOrDetails(selectedBook)}
+      </div>
+    );     
+  }
+  
 }
 
 const mapStateToProps = (state) => {
-    return {
-        books: state.books,
-        selected: state.selected
-    }    
+  return {
+    books: state.books,
+    selected: state.selected
+  }    
 }
 
 const mapDispatchToProps = {
-    updateSelectedBook: updateSelectedBook,
-    clearSelectedBook: clearSelectedBook,
-    decrementPageNumber: decrementPageNumber,
-    incrementPageNumber: incrementPageNumber
+  updateSelectedBook: updateSelectedBook,
+  clearSelectedBook: clearSelectedBook,
+  decrementPageNumber: decrementPageNumber,
+  incrementPageNumber: incrementPageNumber
 }
 
 const ViewControllerContainer = connect(mapStateToProps,mapDispatchToProps)(ViewController);
